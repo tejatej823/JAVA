b0195123cf535364b46d4d75bb8ba7aebcad584a
virat virat18 <virat18teja18@email.com>
2026-02-14 20:41:19 +0530

    Second commit

--- JAVA_BASIC/Collections/List/Array_List.java
+++ JAVA_BASIC/Collections/List/Array_List.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class List_example{
+public class Array_List{
     public static void main(String[] args){
         List<Integer>list1=new ArrayList<Integer>();
         List<Integer>list2=new ArrayList<Integer>();
@@ -16,6 +16,11 @@ public class List_example{
         list1.remove(1);
         System.out.println("list1 : "+list1);
         list1.set(2,11);
+        System.out.println("list1 : "+list1);
+        list1.add(0,99);
+        System.out.println("list1 : "+list1);
+        list1=new LinkedList<Integer>(list2);
+        list1.add(0,88);
         System.out.println("list1 : "+list1);
     }
 }
---
+++ JAVA_BASIC/Collections/List/Linked_List.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class Linked_List{
+    public static void main(String[] args){
+        List<String> list1=new LinkedList<>();
+        a1.add
+    }
+}
