c4fcd0084ec8437412c6b374516c3298f14b57d4
virat virat18 <virat18teja18@email.com>
2026-02-14 20:53:01 +0530

    Updated project today

--- JAVA_BASIC/Collections/List/Linked_List.java
+++ JAVA_BASIC/Collections/List/Linked_List.java
@@ -2,6 +2,6 @@ import java.util.*;
 public class Linked_List{
     public static void main(String[] args){
         List<String> list1=new LinkedList<>();
-        a1.add
+        a1.add(34);
     }
 }
