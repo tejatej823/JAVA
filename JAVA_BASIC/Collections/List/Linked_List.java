ca861f2f4a20ec1085058f4946e02a262a3b90b7
virat virat18 <tejagudditi@gmail.com>
2026-02-15 21:11:15 +0530

    Updated LinkedList

--- JAVA_BASIC/Collections/List/Linked_List.java
+++ JAVA_BASIC/Collections/List/Linked_List.java
@@ -1,7 +1,10 @@
 import java.util.*;
 public class Linked_List{
     public static void main(String[] args){
-        List<String> list1=new LinkedList<>();
-        a1.add(34);
+        List<Integer> list1=new LinkedList<>();
+        list1.add(34);
+        List<Integer>list2=new ArrayList<>(list1);
+        list2.add(33);
+        System.out.println(list2);
     }
 }
