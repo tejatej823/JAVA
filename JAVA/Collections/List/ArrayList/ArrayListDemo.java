648291b78eeb3bd2bcf721a8c7f828d283e53c44
virat virat18 <tejagudditi@gmail.com>
2026-02-25 22:52:45 +0530

    Added ArrayList

---
+++ JAVA/Collections/List/ArrayList/ArrayListDemo.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class ArrayListDemo{
+    public static void main(String[] args){
+        List<Integer>list1=new ArrayList<>();
+        list1.add(12);
+        list1.add(23);
+        list1.add(34);
+        list1.add(45);
+        System.out.println(list1);       
+    }
+}
