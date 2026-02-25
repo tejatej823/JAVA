fefa03419d784838212d0629d33153b7cc8f055d
virat virat18 <tejagudditi@gmail.com>
2026-02-26 03:55:29 +0530

    ArrayList Advanced

--- JAVA/Collections/List/ArrayList/ArrayListDemo.java
+++ JAVA/Collections/List/ArrayList/ArrayListDemo.java
@@ -6,6 +6,40 @@ public class ArrayListDemo{
         list1.add(23);
         list1.add(34);
         list1.add(45);
-        System.out.println(list1);       
+        System.out.println(list1); 
+        list1.add(0,99);
+        System.out.println(list1); 
+        System.out.println(list1.get(0));
+        list1.set(0,22);
+        System.out.println(list1);
+        list1.remove(3);
+        System.out.println(list1);
+        System.out.println(list1.remove(Integer.valueOf(2)));
+        System.out.println(list1.size());
+        for(int i=0;i<list1.size();i++){
+            System.out.print(list1.get(i)+" ");
+        }
+        for(Integer i:list1){
+            System.out.print(i+" ");
+        }
+        Iterator<Integer>it=list1.iterator();
+        while(it.hasNext()){
+            System.out.println(it.next());
+        }
+        for(int i=0;i<8;i++){
+            list1.add(i);
+        }
+        System.out.println(list1.size());
+        list1.add(null);
+        System.out.println(list1.get(12));
+        List<Integer>list2=new ArrayList<>();
+        list2.add(999);
+        list2.addAll(1,list1);
+        System.out.println(list2);
+        System.out.println(list2.indexOf(23));
+        list2.add(45);
+        System.out.println(list2.lastIndexOf(45));
+        System.out.println(list2.subList(2,6));
+        
     }
 }
