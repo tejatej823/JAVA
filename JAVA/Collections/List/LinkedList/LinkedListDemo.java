82516730e16a4af2961ffb01055649113ab46828
virat virat18 <tejagudditi@gmail.com>
2026-02-27 20:13:45 +0530

    LinkedList added

---
+++ JAVA/Collections/List/LinkedList/LinkedListDemo.java
@@ -0,0 +1,10 @@
+import java.util.LinkedList;
+
+public class LinkedListDemo{
+    public static void main(String[] args){
+        LinkedList<String> list = new LinkedList<>();
+list.add("Java");
+list.add("Python");
+System.out.println(list);
+    }
+}
