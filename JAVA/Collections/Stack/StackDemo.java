bb5e6e1c30648904c601e5bab39503cdce55204f
virat virat18 <tejagudditi@gmail.com>
2026-03-01 21:12:52 +0530

    Stack added

---
+++ JAVA/Collections/Stack/StackDemo.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class StackDemo{
+    public static void main(String[] args){
+        Stack<Integer>stack=new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+        System.out.println(stack);
+        stack.pop();
+        stack.pop();
+        stack.pop();
+        System.out.println(stack);            
+    }
+}
