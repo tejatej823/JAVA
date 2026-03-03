6b9850667ef77fd7c99cbbd571963a776eef6d95
virat virat18 <tejagudditi@gmail.com>
2026-03-03 22:34:40 +0530

    Set interface

---
+++ JAVA/Collections/Set/SetDemo.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class SetDemo{
+    public static void main(String[] args){
+        Set<String>set=new HashSet<>();
+        set.add("Java");
+        set.add("Python");
+        set.add("C++");
+        System.out.println(set);
+    }
+}
