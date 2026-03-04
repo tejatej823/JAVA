5c6ab3ca5df87761c257ee0d4267385d825cf7bd
virat virat18 <tejagudditi@gmail.com>
2026-03-04 23:13:12 +0530

    Set updated

--- JAVA/Collections/Set/SetDemo.java
+++ JAVA/Collections/Set/SetDemo.java
@@ -1,10 +1,17 @@
 import java.util.*;
 public class SetDemo{
     public static void main(String[] args){
-        Set<String>set=new HashSet<>();
-        set.add("Java");
-        set.add("Python");
-        set.add("C++");
+        Set<Integer>set=new HashSet<>();
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        set.add(4);
+        System.out.println(set);
+        set.remove(3);
+        System.out.println(set);
+        System.out.println(set.size());
+        System.out.println(set.isEmpty());
+        set.clear();
         System.out.println(set);
     }
 }
