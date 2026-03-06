7c356721df5481fda8eb321cc19ddef6906a2ec7
virat virat18 <tejagudditi@gmail.com>
2026-03-07 04:34:16 +0530

    Queue added

---
+++ JAVA/Collections/Queue/Dequeue/ArrayDequeDemo.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class ArrayDequeDemo{
+    public static void main(String[] args){
+        ArrayDeque<Integer>ad=new ArrayDeque<>();
+        System.out.println(ad.size());
+    }
+}
