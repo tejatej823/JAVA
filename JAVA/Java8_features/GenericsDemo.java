d9d0819f7ca58fd86d1e96489f092368bfeb812a
virat virat18 <tejagudditi@gmail.com>
2026-03-09 23:51:33 +0530

    Generics

---
+++ JAVA/Java8_features/GenericsDemo.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class GenericsDemo{
+    public static void main(String[] args){
+        List list=new ArrayList();
+        list.add(10);
+        list.add("String");
+        System.out.println(list);
+}}
